e11c811257fad203703b387717e34b964ab4fd8f
Imran Khan <109821243+imrancrow93@users.noreply.github.com>
2023-07-16 17:24:54 +0600

    fix store prlm

--- src/redux-store/reducers/BookMark/index.js
+++ src/redux-store/reducers/BookMark/index.js
@@ -1,3 +1,37 @@
+// import {SAVE_BOOKMARK} from '../../constants/ActionTypes';
+
+// const initialState = {
+//   bookmarkList: [],
+//   categoryList: [
+//     {label: 'Category A', value: 'Category A'},
+//     {label: 'Category B', value: 'Category B'},
+//     {label: 'Category C', value: 'Category C'},
+//     {label: 'Category E', value: 'Category E'},
+//     {label: 'Category F', value: 'Category F'},
+//     {label: 'Category G', value: 'Category G'},
+//   ],
+// };
+
+// const index = (state = initialState, action) => {
+//   switch (action.type) {
+//     case SAVE_BOOKMARK:
+//       return {
+//         ...state,
+//         bookmarkList: [...state.bookmarkList, action.payload],
+//       };
+//     default:
+//       return {
+//         ...state,
+//         bookmarkList: state.bookmarkList.sort((a, b) => {
+//           // Replace 'createdAt' with the property you want to sort by
+//           return new Date(b.createdAt) - new Date(a.createdAt);
+//         }),
+//       };
+//   }
+// };
+
+// export default index;
+
 import {SAVE_BOOKMARK} from '../../constants/ActionTypes';
 
 const initialState = {
@@ -12,18 +46,18 @@ const initialState = {
   ],
 };
 
-const reducers = (state = initialState, action) => {
+const index = (state = initialState, action) => {
   switch (action.type) {
     case SAVE_BOOKMARK:
+      const newBookmarkList = [...state.bookmarkList, action.payload];
+      newBookmarkList.sort((a, b) => a.title.localeCompare(b.title)); // Sort the bookmarkList array by title
       return {
         ...state,
-        bookmarkList: [...state.bookmarkList, action.payload],
+        bookmarkList: newBookmarkList,
       };
     default:
-      return {
-        ...state,
-      };
+      return state;
   }
 };
 
-export default reducers;
+export default index;
